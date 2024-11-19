c106cc38f5ee5b5ae2c05edbc7cc7004547e39b1
asu <antonio.popa.dev@gmail.com>
2024-11-19 20:19:17 +0200

    Added usb port component

--- src/app/pages/scheme-pini/usb-port/usb-port.component.ts
+++ src/app/pages/scheme-pini/usb-port/usb-port.component.ts
@@ -9,33 +9,29 @@ import { ModalController } from '@ionic/angular';
 export class UsbPortComponent {
 
   usb2Pins = [
-    { pin: 1, name: 'Vcc', description: 'Power (5V)' },
-    { pin: 2, name: 'D-', description: 'Data -' },
-    { pin: 3, name: 'D+', description: 'Data +' },
-    { pin: 4, name: 'GND', description: 'Ground' },
+    { pin: 1, name: 'VCC', description: '+5 V', color: 'Red' },
+    { pin: 2, name: 'D-', description: 'Data -', color: 'White' },
+    { pin: 3, name: 'D+', description: 'Data +', color: 'Green' },
+    { pin: 4, name: 'GND', description: 'Ground', color: 'Black' },
   ];
 
   usb3Pins = [
-    { pin: 1, name: 'Vcc', description: 'Power (5V)' },
-    { pin: 2, name: 'D-', description: 'Data -' },
-    { pin: 3, name: 'D+', description: 'Data +' },
-    { pin: 4, name: 'GND', description: 'Ground' },
-    { pin: 5, name: 'SSR-', description: 'SuperSpeed Receive -' },
-    { pin: 6, name: 'SSR+', description: 'SuperSpeed Receive +' },
-    { pin: 7, name: 'SST-', description: 'SuperSpeed Transmit -' },
-    { pin: 8, name: 'SST+', description: 'SuperSpeed Transmit +' },
+    { pin: 1, typeA: 'Vbus', typeB: 'Vbus', signal: 'Power', description: 'USB Power', color: 'Red' },
+    { pin: 2, typeA: 'D-', typeB: 'D-', signal: 'Data -', description: 'USB 2.0 Data -', color: 'White' },
+    { pin: 3, typeA: 'D+', typeB: 'D+', signal: 'Data +', description: 'USB 2.0 Data +', color: 'Green' },
+    { pin: 4, typeA: 'GND', typeB: 'GND', signal: 'Ground', description: 'Ground for Power', color: 'Black' },
+    { pin: 5, typeA: 'SSTX+', typeB: 'SSTX+', signal: 'Transmit +', description: 'SuperSpeed Transmit +', color: 'Orange' },
+    { pin: 6, typeA: 'SSTX-', typeB: 'SSTX-', signal: 'Transmit -', description: 'SuperSpeed Transmit -', color: 'Purple' },
   ];
 
   usbTypeCPins = [
-    { pin: 'A1/B1', name: 'GND', description: 'Ground' },
-    { pin: 'A2/B2', name: 'TX+', description: 'SuperSpeed Transmit +' },
-    { pin: 'A3/B3', name: 'TX-', description: 'SuperSpeed Transmit -' },
-    { pin: 'A4/B4', name: 'Vcc', description: 'Power (5V)' },
-    { pin: 'A5/B5', name: 'CC', description: 'Configuration Channel' },
-    { pin: 'A6/B6', name: 'D+', description: 'USB 2.0 Data +' },
-    { pin: 'A7/B7', name: 'D-', description: 'USB 2.0 Data -' },
-    { pin: 'A8/B8', name: 'RX-', description: 'SuperSpeed Receive -' },
-    { pin: 'A9/B9', name: 'RX+', description: 'SuperSpeed Receive +' },
+    { pin: 'A1/B12', signal: 'GND', description: 'Ground' },
+    { pin: 'A2/B11', signal: 'TX+', description: 'SuperSpeed Transmit +' },
+    { pin: 'A3/B10', signal: 'TX-', description: 'SuperSpeed Transmit -' },
+    { pin: 'A4/B9', signal: 'Vbus', description: 'USB Power' },
+    { pin: 'A5/B8', signal: 'CC', description: 'Configuration Channel' },
+    { pin: 'A6/B7', signal: 'D+', description: 'USB 2.0 Data +' },
+    { pin: 'A7/B6', signal: 'D-', description: 'USB 2.0 Data -' },
   ];
 
   constructor(private modalController: ModalController) {}
